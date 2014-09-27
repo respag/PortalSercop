cda7b9e28cedf3db469593acdfc2f3c7b2e4a041
respag <respag@hotmail.com>
2014-09-27 09:52:32 -0500

    Primer Commit

---
+++ UltimusSercopPortal/Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ultimus.WFServer;
+
+
+namespace ULAPW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string BLUP, string Aplicacion)
+        {             
+            if (BLUP != null && Aplicacion =="MA")
+            {
+                var flag = Convert.ToBoolean(ConfigurationManager.AppSettings["FlagUltimusLogin"]);
+                var ip = Request.UserHostAddress;
+                
+                if (IsTokenValido(BLUP, ip) )
+                    return View("Initiate");
+                else
+                {
+                    if (!flag)
+                        return Redirect("http://192.168.9.212/easywebacceso/EasyLogin/wfLogin.aspx");
+                    else
+                        return RedirectToAction("Login");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public ActionResult Initiate(bool userLoged = false)
+        {
+            if (userLoged)
+                return View();
+             else
+                return RedirectToAction("Login");
+        }
+
+        private bool IsTokenValido(string token, string ip)
+        {
+            var svc = new ULAPW.ServiceEasyLogin.WSEasyLoginSoapClient();
+            //var ip = Request.UserHostAddress; 
+            var resp = svc.VerificaToken(token, "MA", ip);
+            if (resp == "ERROR" || resp == "TOKEN_NO_VALIDO" || resp == "TOKEN_EXPIRADO" || resp == "IP_INCORRECTA")
+                return false;
+            else
+            {
+                //userLoged = true;
+                ViewBag.Resultado = resp;
+                ViewBag.Ip = ip;
+                return true;
+            }
+            //return true;
+        }
+
+        //public ActionResult Redirige()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Inbox(string BLUP, string Aplicacion)
+        {
+            if (BLUP != null && Aplicacion == "MA")
+            {
+                var flag = Convert.ToBoolean(ConfigurationManager.AppSettings["FlagUltimusLogin"]);
+                var ip = Request.UserHostAddress;
+
+                if (IsTokenValido(BLUP, ip))
+                    return View("Inbox");
+                else
+                {
+                    if (!flag)
+                        return Redirect("http://192.168.9.212/easywebacceso/EasyLogin/wfLogin.aspx");
+                    else
+                        return RedirectToAction("Login");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
+        public ActionResult Completed(string BLUP, string Aplicacion)
+        {
+            if (BLUP != null && Aplicacion == "MA")
+            {
+                var flag = Convert.ToBoolean(ConfigurationManager.AppSettings["FlagUltimusLogin"]);
+                var ip = Request.UserHostAddress;
+
+                if (IsTokenValido(BLUP, ip))
+                    return View("Completed");
+                else
+                {
+                    if (!flag)
+                        return Redirect("http://192.168.9.212/easywebacceso/EasyLogin/wfLogin.aspx");
+                    else
+                        return RedirectToAction("Login");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
+        [OutputCache(Duration=0)]
+        public ActionResult MuestraImagen(string processName, int incidente, int version)
+        {
+            byte[] bytesGif  = GetImageBytes(processName, incidente, version);
+            if (bytesGif != null)
+            {
+                string imageBase64Data = Convert.ToBase64String(bytesGif);
+                string imageDataURL = string.Format("data:image/png;charset=utf-8;base64,{0}", Convert.ToBase64String(bytesGif));
+                ViewBag.ImageData = imageDataURL;
+            }
+            return View();
+        }
+
+
+        public byte[] GetImageBytes(string processname, int incident, int version)
+        {
+            byte[] ret;
+            Incident.Status status = new Incident.Status();
+            status.GetGraphicalStatus(processname, incident, version, out ret);
+
+            return ret;
+        }
+
+
+    }
+}
---
+++ UltimusSercopPortal/Scripts/app/scriptInitiate.js
@@ -0,0 +1,93 @@
+﻿/// <reference path="../jquery-1.10.2.js" />
+/// <reference path=""~/Scripts/DataTables-1.10.2/jquery.dataTables.min.js"/>
+var domain = "soce.int";
+var usr = "alfresco";
+
+$(document).ready(function () {
+    var flag = true;
+    $.getJSON("http://192.168.110.10/InitiateApi/api/Initiates/" + domain + "/" + usr, function (data) {
+        var obj = jQuery.parseJSON(data)
+
+        var oTable = $('#example').dataTable({
+            language: {
+                processing: "Procesando...",
+                search: " ",
+                lengthMenu: "Mostrando _MENU_ registros",
+                info: "Registros <b style='color:red'>_START_ a _END_</b> de un total de <b class='medalla'>_TOTAL_</b>",
+                infoEmpty: "No hay registros para mostrar.",
+                infoFiltered: "(filtrado de _MAX_ registros en total)",
+                infoPostFix: "",
+                loadingRecords: "Cargando registros...",
+                zeroRecords: "No hay registros para mostrar.",
+                emptyTable: "No hay datos disponibles en la tabla",
+                paginate: {
+                    first: "Primero",
+                    previous: "Anterior",
+                    next: "Siguiente",
+                    last: "Ultimo"
+                },
+                aria: {
+                    sortAscending: ": activar para ordenar la columna ascendentemente",
+                    sortDescending: ": activar para ordenar la columna descendentemente"
+                }
+            },
+            "fnDrawCallback": function (oSettings) {
+                $("#initiateLargo").text($("#example_wrapper #example_info b.medalla").text());
+                $("#initiateLargo").css("visibility", "visible");
+
+                $("#example_wrapper #example_filter input").css({
+                    "border": "1px solid silver", "border-radius": "4px", "border-top-right-radius": "0",
+                    "border-bottom-right-radius": "0", "height": "34px", "Width": "196px", "position": "relative", "top": "2px"
+                });
+
+
+                // Con el flag evitamos que se cargue la imagen de la lupa cada vez qu ese dibuja la tabla y solo lo hace la primera vez
+                if (flag) {
+                    $("#example_wrapper #example_filter input").after("<button class='btn btn-default' type='submit'>" +
+                        "<span class='glyphicon glyphicon-search' aria-hidden='true'></span></button>");
+                    flag = false
+                }
+
+                //Aplica estilos al select para paginación para que sea parecido al input de busqueda
+                $("#example_length label select").css({
+                    "border": "1px solid silver", "border-top-left-radius": "4px", "border-top-right-radius": "0",
+                    "border-bottom-right-radius": "0", "border-bottom-left-radius": "4px", "height": "34px", "position": "relative", "top": "2px",
+                    "padding": "6px 12px", "color": "rgb(85, 85, 85)", "font-size": "14px"
+                });
+
+                // Pinta de celeste claro las barras 
+                $("#example_wrapper").css("background-color", "lightsteelblue");
+
+                // Coloca el placeholder Buscar al input
+                $("#example_wrapper #example_filter input").attr("placeholder", "Buscar");
+
+                // Aplica 4 estilos al cuadro de texto de busqueda (el padding, el autofoco, el color y el tamaño de la fuente)
+                $("#example_wrapper #example_filter input").css({ "padding": "6px 12px", "autofocus": "true", "color": "#555555", "font-size": "14px" });
+
+                $("#example_wrapper #example_length").css({ "float": "left" });
+
+                $("#example_wrapper #example_filter").css("float", "right");
+            },
+            "aLengthMenu": [[5, 10, 25, 50], [5, 10, 25, 50]],
+            "iDisplayLength": 10,
+            "aaData": obj,
+            "order": [],
+            "aoColumns": [
+                 { "mData": "NombreProceso" },
+                 { "mData": "VersionProceso", "sClass": "alignCenter" },
+                 { "mData": "NombreTarea" },
+                 { "mData": "Url" }
+            ],
+        });
+    });
+
+    $('#example thead, #example tfoot').css({ "background-color": "#202020", "color": "white" });
+
+    $('#example').on('click', 'tbody tr', function (event) {
+        // var dir = ($(this).find('td:last').text()).replace('.', 'http://192.168.110.10/UltWeb');
+        var tId = ($(this).find('td:last').text()).substring(52, 83);
+        // alert(tId);
+        var dir = "http://192.168.110.10/Ultimus.Sercop.Compartidos/FrmUltimus.aspx?UserID=" + domain + "/" + usr + "&TaskID=" + tId;
+        window.open(dir, "newWindow", "height=" + screen.height + ", width =" + screen.width);
+    });
+});
